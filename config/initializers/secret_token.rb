28a1251f437123bb73acfb4962b56634594727b9
unknown <jin@jin-w700.(none)>
2012-03-06 22:04:56 -0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mytiny::Application.config.secret_token = '7c980f97aa4b680c9645489aa5f97bfbb8e3af2acebcf8cdd7d5236cf08d69bb06284f33203768e18895e29fcc55a33be1a85629f7b569cbd7dbbf2586d1461e'
