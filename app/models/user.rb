26e0bfecf494827a4060241090f51582a8f53f9b
unknown <jin@jin-w700.(none)>
2012-11-07 22:43:19 -0800

    finished additional tests on user

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,7 +12,7 @@ class User < ActiveRecord::Base
   validates :email, presence: true,
             format: {with: VALID_EMAIL_REGEX},
             uniqueness: {case_sensitive: false}
-  validates :password, presence:true, length: {minimum: 6}, confirmation: true
+  validates :password, length: {minimum: 6}, confirmation: true
   validates :password_confirmation, presence: true
 
   #attr_accessor :password_confirmation, :name, :email
