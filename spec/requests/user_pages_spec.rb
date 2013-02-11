d53dc77451c740de2be6ebba7cd815c56b2b4278
unknown <jin@jin-w500.(none)>
2013-02-10 18:19:22 -0800

    finished follower exercise

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -64,6 +64,16 @@ describe "User pages" do
     it {should have_selector('h1', text: user.name)}
     it {should have_selector('title', text: user.name)}
 
+    describe "follower/following counts" do
+      let(:other_user) {FactoryGirl.create(:user)}
+      before do
+        other_user.follow!(user)
+        visit user_path(user)
+      end
+      it {should have_link("0 following", href: following_user_path(user))}
+      it {should have_link("1 followers", href: followers_user_path(user))}
+    end
+
     describe "for users with microposts" do
       before { sign_in user }
       it {should have_content(m1.content)}
