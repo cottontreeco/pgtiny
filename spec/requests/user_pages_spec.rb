b1a731a93c74d70355abc0905acb63e323ad29ba
cottontreeco <jin8121@yahoo.com>
2014-06-08 23:00:37 -0700

    finish follow and unfollow button

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -89,7 +89,7 @@ describe "User pages" do
     end
   end
 
-  describe "Profile page" do
+  describe "profile page" do
     let(:user) { FactoryGirl.create(:user)}
     let!(:m1) {FactoryGirl.create(:review, user: user, remark: "Foo")}
     let!(:m2) {FactoryGirl.create(:review, user: user, remark: "Bar")}
@@ -104,6 +104,50 @@ describe "User pages" do
       it {should have_content(m1.remark)}
       it {should have_content(m2.remark)}
       it {should have_content(user.reviews.count)}
+    end
+
+    describe "follow/unfollow buttons" do
+      let(:other_user) {FactoryGirl.create(:user)}
+      before {valid_signin user}
+
+      describe "following a user" do
+        before {visit user_path(other_user)}
+        it "should increment the followed user count" do
+          expect do
+            click_button "Follow"
+          end.to change(user.followed_users, :count).by(1)
+        end
+        it "should increment the other user's followers count" do
+          expect do
+            click_button "Follow"
+          end.to change(other_user.followers, :count).by(1)
+        end
+        describe "toggling the button" do
+          before {click_button "Follow"}
+          it {should have_xpath("//input[@value='Unfollow']")}
+        end
+      end
+
+      describe "unfollowing a user" do
+        before do
+          user.follow!(other_user)
+          visit user_path(other_user)
+        end
+        it "should decrement the followed user count" do
+          expect do
+            click_button "Unfollow"
+          end.to change(user.followed_users, :count).by(-1)
+        end
+        it "should decrement the other user's followers count" do
+          expect do
+            click_button "Unfollow"
+          end.to change(other_user.followers, :count).by(-1)
+        end
+        describe "toggling the button" do
+          before {click_button "Unfollow"}
+          it {should have_xpath("//input[@value='Follow']")}
+        end
+      end
     end
   end
 
